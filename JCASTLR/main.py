dbc73c530a65c5dd4f98ba10cfb8bda7f6066ef7
MikeLippincott <1michaell2017@gmail.com>
2022-09-12 12:20:01 -0600

    paralellization

--- JCASTLR/main.py
+++ JCASTLR/main.py
@@ -2,6 +2,7 @@
 import argparse
 import insilico_translation as ist
 import longread_functions as lrf
+import post_run_stats as prs
 import pandas as pd
 import numpy as np
 from gtfparse import read_gtf
@@ -15,6 +16,7 @@ import os
 from time import sleep
 from joblib import Parallel, delayed
 import multiprocessing
+import threading
 
 # Main function
 def main():
@@ -28,146 +30,190 @@ def main():
     parser.add_argument('-o', '--outpath', help='Output path. Note JCASTLR autonames files', required=True)
     args = parser.parse_args()
     # set constants
-    gtf =  args.gtf
+    gtf = args.gtf
     bed = args.bed
     fasta = args.fasta
     prefix = args.file_prefix
     out_location = args.outpath
     g = ist.Gtf(gtf)
     b = ist.Bed(bed)
+    duplicate_count = 0
     # define how many reads need to be looped through and level lists
+    # with open(fasta) as f:
+    # Canonical = []
+    # L1 = []
+    # L2 = []
+    # L3 = []
+    # L4 = []
+    # L5 = []
     with open(fasta) as f:
-        Canonical = []
-        L1 = []
-        L2 = []
-        L3 = []
-        L4 = []
-        L5 = []
-        n = 0
-        n1 = 0
-        for line in f:
-            if line.startswith(">"):
-                n += 1
+        number_of_records = 0
+        for record in SeqIO.parse(f, 'fasta'):
+            number_of_records += 1
+    print(f'{number_of_records} transcripts to process.')
     # loop through each record to get info and translate
     with open(fasta) as f:
+        # num_cores = multiprocessing.cpu_count()
+        # print(num_cores)
+        # results = Parallel(n_jobs=num_cores)(delayed(paralell_me)(record,g,out_location, prefix) for record in SeqIO.parse(f, 'fasta'))
+        # print(results)
+        number_of_records = 0
+        threads = list()
         for record in SeqIO.parse(f, 'fasta'):
-            n1 += 1
-            lrf.progress_bar(n1, n, 50)
-            r = record
-            s = ist.Sequences(g, r)
-            s.subset_gtf()
-            s.get_meta()
-            conditions = ['Canonical','L1']
-            if any(conditions) and not all(conditions):
-                s.annotated_trancript_trim()
-            # a = ist.Canonical_test(s)
-            # a.get_canonical_aa()
-            # a.make_header()
-            # print(a.level, a.rid, a.biotype)
-            if s.level == "Canonical":
-                # a = ist.Canonical_test(s)
-                # a.get_canonical_aa()
-                # a.make_header()
-                # s.annotated_trancript_trim()
-                p = ist.Peptide(s)
-                prot_seq = p.annotated_translate()
-                # p.get_canonical_aa_uniprot_local()
-                p.make_header()
-                seq = p.str_to_seqrec(prot_seq)
-                Canonical.append(seq)
-            elif s.level == "L1":
-                # a = ist.Canonical_test(s)
-                # a.get_canonical_aa()
-                # a.make_header()
-                s.annotated_trancript_trim()
-                p = ist.Peptide(s)
-                prot_seq = p.annotated_translate()
-                # p.get_canonical_aa_uniprot_local()
-                p.make_header()
-                # print(prot_seq)
-                seq = p.str_to_seqrec(prot_seq)
-                L1.append(seq)
-            elif s.level == "L2":
-                # a = ist.Canonical_test(s)
-                # a.get_canonical_aa()
-                # a.make_header()
-                # s.annotated_trancript_trim()
-                p = ist.Peptide(s)
-                prot_seq = p.multi_phase_translate()
-                # p.get_canonical_aa_uniprot_local()
-                p.make_header()
-                seq = p.str_to_seqrec(prot_seq)
-                L2.append(seq)
-            elif s.level == "L3":
-                # a = ist.Canonical_test(s)
-                # a.get_canonical_aa()
-                # a.make_header()
-                p = ist.Peptide(s)
-                prot_seq = p.multi_phase_translate()
-                # p.get_canonical_aa_uniprot_local()
-                p.make_header()
-                seq = p.str_to_seqrec(prot_seq)
-                L3.append(seq)
-            elif s.level == "L4":
-                # a = ist.Canonical_test(s)
-                # a.get_canonical_aa()
-                # a.make_header()
-                p = ist.Peptide(s)
-                prot_seq = p.multi_phase_translate()
-                # p.get_canonical_aa_uniprot_local()
-                p.make_header()
-                seq = p.str_to_seqrec(prot_seq)
-                L4.append(seq)
-            elif s.level == "L5":
-                # a = ist.Canonical_test(s)
-                # a.make_header()
-                p = ist.Peptide(s)
-                prot_seq = p.multi_phase_translate()
-                # p.get_canonical_aa_uniprot_local()
-                p.make_header()
-                seq = p.str_to_seqrec(prot_seq)
-                L5.append(seq)
-            else:
-                print("Orphan Read")
+            number_of_records += 1
+            x = threading.Thread(target=paralell_me, args=(record,g,out_location,prefix,))
+            threads.append(x)
+            x.start()
+    prs.post_run_counts(out_location,prefix)
+    print(f'{time.perf_counter() - start} seconds')
 
-            ph = ist.Post_hoc_reassignment(s,p)
-            ph.get_canonical_aa_uniprot_local('resources/DB/reviewed_canonical.fasta')
-            ph.make_header()
+def paralell_me(record,g,out_location, prefix):
+    # n1 += 1
+    # lrf.progress_bar(n1, n, 50)
+    r = record
+    s = ist.Sequences(g, r)
+    s.subset_gtf()
+    s.get_meta()
+    # a = ist.Canonical_test(s)
+    # a.get_canonical_aa()
+    # a.make_header()
+    # print(a.level, a.rid, a.biotype)
+    if s.level == "Canonical":
+        # a = ist.Canonical_test(s)
+        # a.get_canonical_aa()
+        # a.make_header()
+        s.annotated_trancript_trim()
+        p = ist.Peptide(s)
+        prot_seq = p.annotated_translate()
+        # p.get_canonical_aa_uniprot_local()
+        p.make_header()
+        # seq = p.str_to_seqrec(prot_seq)
+        # Canonical.append(seq)
+    elif s.level == "L1":
+        # a = ist.Canonical_test(s)
+        # a.get_canonical_aa()
+        # a.make_header()
+        s.annotated_trancript_trim()
+        p = ist.Peptide(s)
+        prot_seq = p.annotated_translate()
+        # p.get_canonical_aa_uniprot_local()
+        p.make_header()
+        # print(prot_seq)
+        # seq = p.str_to_seqrec(prot_seq)
+        # L1.append(seq)
+    elif s.level == "L2":
+        # a = ist.Canonical_test(s)
+        # a.get_canonical_aa()
+        # a.make_header()
+        s.annotated_trancript_trim()
+        p = ist.Peptide(s)
+        p1 = p.annotated_translate()
+        p2 = p.multi_phase_translate()
+        if len(p1) > len(p2):
+            p.prot = p1
+        elif len(p1) < len(p2):
+            p.prot = p2
+        else:
+            p.prot = p1
+        # p.get_canonical_aa_uniprot_local()
+        p.make_header()
+        # seq = p.str_to_seqrec(prot_seq)
+        # L2.append(seq)
+    elif s.level == "L3":
+        # a = ist.Canonical_test(s)
+        # a.get_canonical_aa()
+        # a.make_header()
+        p = ist.Peptide(s)
+        prot_seq = p.multi_phase_translate()
+        # p.get_canonical_aa_uniprot_local()
+        p.make_header()
+        # seq = p.str_to_seqrec(prot_seq)
+        # L3.append(seq)
+    elif s.level == "L4":
+        # a = ist.Canonical_test(s)
+        # a.get_canonical_aa()
+        # a.make_header()
+        p = ist.Peptide(s)
+        prot_seq = p.multi_phase_translate()
+        # p.get_canonical_aa_uniprot_local()
+        p.make_header()
+        # seq = p.str_to_seqrec(prot_seq)
+        # L4.append(seq)
+    elif s.level == "L5":
+        # a = ist.Canonical_test(s)
+        # a.make_header()
+        p = ist.Peptide(s)
+        prot_seq = p.multi_phase_translate()
+        # p.get_canonical_aa_uniprot_local()
+        p.make_header()
+        # seq = p.str_to_seqrec(prot_seq)
+        # L5.append(seq)
+    else:
+        print("Orphan Read")
 
-            # post hoc change of level
-            if ph.level == 'Canonical':
-                if ph.old == 'L1':
-                    # val = L1[-1]
-                    seq = ph.str_to_seqrec()
-                    # print("here   ",val)
-                    Canonical.append(seq)
-                    L1 = L1[:-1]
-                elif ph.old == 'L2':
-                    # val = L2[-1]
-                    seq = ph.str_to_seqrec()
-                    Canonical.append(seq)
-                    L2 = L2[:-1]
-                elif ph.old == 'L3':
-                    # val = L3[-1]
-                    seq = ph.str_to_seqrec()
-                    Canonical.append(seq)
-                    L3 = L3[:-1]
-                elif ph.old == 'L4':
-                    # val = L4[-1]
-                    seq = ph.str_to_seqrec()
-                    Canonical.append(seq)
-                    L4 = L4[:-1]
-                elif ph.old == 'L5':
-                    # val = L5[-1]
-                    seq = ph.str_to_seqrec()
-                    Canonical.append(seq)
-                    L5 = L5[:-1]
-                else:
-                    print("error post hoc")
-            elif ph.level != 'Canonical':
-                ph.get_aa_uniprot_local('resources/DB/reviewed_included_isoforms.fasta')
-                ph.make_header()
-                # seq = ph.str_to_seqrec()
+    ph = ist.Post_hoc_reassignment(s, p)
+    ph.get_canonical_aa_uniprot_local('resources/DB/reviewed_canonical.fasta')
+    ph.make_header()
+
+    # post hoc change of level
+    if ph.level == 'Canonical':
+        seq = ph.str_to_seqrec()
+        lrf.prot_to_fasta(seq, out_location, prefix, "_Canonical")
+
+        # if ph.old == 'L1':
+        #     # val = L1[-1]
+        #     seq = ph.str_to_seqrec()
+        #     # print("here   ",val)
+        #     lrf.prot_to_fasta(seq, out_location, prefix, "_Canonical")
+        # elif ph.old == 'L2':
+        #     # val = L2[-1]
+        #     seq = ph.str_to_seqrec()
+        #     lrf.prot_to_fasta(seq, out_location, prefix, "_Canonical")
+        # elif ph.old == 'L3':
+        #     # val = L3[-1]
+        #     seq = ph.str_to_seqrec()
+        #     lrf.prot_to_fasta(seq, out_location, prefix, "_Canonical")
+        # elif ph.old == 'L4':
+        #     # val = L4[-1]
+        #     seq = ph.str_to_seqrec()
+        #     lrf.prot_to_fasta(seq, out_location, prefix, "_Canonical")
+        # elif ph.old == 'L5':
+        #     # val = L5[-1]
+        #     seq = ph.str_to_seqrec()
+        #     lrf.prot_to_fasta(seq, out_location, prefix, "_Canonical")
+        # else:
+        #     print("error post hoc")
+    elif ph.level != 'Canonical':
+        ph.get_aa_uniprot_local('resources/DB/reviewed_included_isoforms.fasta')
+        ph.make_header()
+        seq = ph.str_to_seqrec()
+        if ph.level == 'L1':
+            # val = L1[-1]
+            # seq = ph.str_to_seqrec()
+            # print("here   ",val)
+            lrf.prot_to_fasta(seq, out_location, prefix, "_Level1")
+        elif ph.level == 'L2':
+            # val = L2[-1]
+            # seq = ph.str_to_seqrec()
+            lrf.prot_to_fasta(seq, out_location, prefix, "_Level2")
+        elif ph.level == 'L3':
+            # val = L3[-1]
+            # seq = ph.str_to_seqrec()
+            lrf.prot_to_fasta(seq, out_location, prefix, "_Level3")
+        elif ph.level == 'L4':
+            # val = L4[-1]
+            # seq = ph.str_to_seqrec()
+            lrf.prot_to_fasta(seq, out_location, prefix, "_Level4")
+        elif ph.level == 'L5':
+            # val = L5[-1]
+            # seq = ph.str_to_seqrec()
+            lrf.prot_to_fasta(seq, out_location, prefix, "_Level5")
+        else:
+            print("error post hoc")
+    else:
+        print("Error")
+
+
     #
     # Canonical_revised = []
     # L1_revised = []
@@ -243,26 +289,26 @@ def main():
 
 
 
-    for i in Canonical:
-        lrf.prot_to_fasta(i, out_location, prefix, "_Canonical")
-    print(f'{len(Canonical)} Canonical Isoforms')
-    for i in L1:
-        lrf.prot_to_fasta(i, out_location, prefix,"_Level1")
-    print(f'{len(L1)} Level 1 Isoforms')
-    for i in L2:
-        lrf.prot_to_fasta(i, out_location,prefix, "_Level2")
-    print(f'{len(L2)} Level 2 Isoforms')
-    for i in L3:
-        lrf.prot_to_fasta(i, out_location, prefix,"_Level3")
-    print(f'{len(L3)} Level 3 Isoforms')
-    for i in L4:
-        lrf.prot_to_fasta(i, out_location, prefix,"_Level4")
-    print(f'{len(L4)} Level 4 Isoforms')
-    for i in L5:
-        lrf.prot_to_fasta(i, out_location, prefix,"_Level5")
-    print(f'{len(L5)} Level 5 Isoforms')
+    # for i in Canonical:
+    #     lrf.prot_to_fasta(i, out_location, prefix, "_Canonical")
+    # print(f'{len(Canonical)} Canonical Isoforms')
+    # for i in L1:
+    #     lrf.prot_to_fasta(i, out_location, prefix,"_Level1")
+    # print(f'{len(L1)} Level 1 Isoforms')
+    # for i in L2:
+    #     lrf.prot_to_fasta(i, out_location,prefix, "_Level2")
+    # print(f'{len(L2)} Level 2 Isoforms')
+    # for i in L3:
+    #     lrf.prot_to_fasta(i, out_location, prefix,"_Level3")
+    # print(f'{len(L3)} Level 3 Isoforms')
+    # for i in L4:
+    #     lrf.prot_to_fasta(i, out_location, prefix,"_Level4")
+    # print(f'{len(L4)} Level 4 Isoforms')
+    # for i in L5:
+    #     lrf.prot_to_fasta(i, out_location, prefix,"_Level5")
+    # print(f'{len(L5)} Level 5 Isoforms')
 
-    print(f'{time.perf_counter() - start} seconds')
+
 
 
 
